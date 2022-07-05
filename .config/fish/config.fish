db70ad7e659980fbaa6b125f3561c407c2cba0b1
Jorgen <jorgen.cani7@outlook.com>
2022-07-05 13:44:17 +0300

    Changes on mpv, fish and kitty

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,3 +5,6 @@ end
 # Aliases
 alias v="nvim"
 alias sv="sudo nvim"
+
+# This is used to spawn a thin wrapper for SSH, as it barely works on its own 
+alias s="kitty +kitten ssh"
--- .config/fish/functions/goodnight.fish
+++ .config/fish/functions/goodnight.fish
@@ -1,6 +1,11 @@
 function goodnight --description 'Upgrade system and shut down. Usaully used before going to sleep'
 	set -l fdate (date '+%Y%m%d-%T')
-	sudo dnf upgrade --refresh | tee ~/Documents/.goodnight-logs/$fdate.log 
-	shutdown | tee ~/Documents/.goodnight-logs/$fdate.log 
-
+	set -l dir ~/Documents/.goodnight-logs   
+	# If directory does not exist, create it
+	if ! test -d $dir 
+		mkdir $dir
+	end
+	touch $dir/$fdate.log
+	sudo dnf upgrade -y --refresh | tee $dir/$fdate.log 
+	shutdown
 end
