86d934b7a230a1030e56f3bdb2a168acb6e909c7
jgenc <jgen.cani@gmail.com>
2023-03-25 23:29:02 +0200

    git aliases for fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,11 +6,20 @@ end
 set -gx EDITOR /usr/bin/vim
 set -gx FLYCTL_INSTALL "/home/jorgen/.fly" 
 
-# Aliases
+# ==================================
+#           Aliases
+# ==================================
+
 #alias v="nvim"
 #alias sv="sudo nvim"
 alias r="ranger"
 
+alias ru="ranger ~/Workspace/university/"
+
 # This is used to spawn a thin wrapper for SSH, as it barely works on its own 
 alias s="kitty +kitten ssh"
 
+# Git
+alias gst="git status"
+alias gp="git push"
+alias ga="git add"
