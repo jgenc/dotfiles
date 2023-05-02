2ccadb711e8d647af3e0ad0eac07dcd92f073847
jgenc <jgen.cani@gmail.com>
2023-05-02 22:04:57 +0300

    feat: nvim Comment plugin

--- .config/nvim/lua/jgen/packer.lua
+++ .config/nvim/lua/jgen/packer.lua
@@ -41,4 +41,11 @@ return require('packer').startup(function(use)
         }
     }
 
+    use {
+        'numToStr/Comment.nvim',
+        config = function()
+            require('Comment').setup()
+        end
+    }
+
 end)
