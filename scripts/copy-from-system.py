0eb63820c885ea9f4e1fcdd1b37a48d7e966d794
jgenc <jgen.cani@gmail.com>
2023-05-05 09:02:11 +0300

    fix: script fix/rewrite

--- scripts/copy-from-system.py
+++ scripts/copy-from-system.py
@@ -3,21 +3,30 @@ import os
 import shutil
 import json
 
-# "directories is the first object in the json"
 dirs = json.load(open("./directories.json", "r"))["directories"]
 
 
-for dir in dirs:
+def copy(dir):
     src = f"{os.environ['HOME']}/.config/{dir}"
-    # TODO:
-    # Test if cwd is the dotfiles repository, don't just run anywhere. 
-    dst = os.path.join(os.getcwd(), f".config/{dir}")
-    dst = dst.replace("/scripts", "")
-
-    print(dst)
+    # TODO: Test if cwd is the dotfiles repository, don't just run anywhere.
+    dst = os.path.join(os.getcwd().replace(
+        "/scripts", ""), f".config/{dir}")
 
     if not os.path.exists(src):
-        continue
+        return
 
     shutil.copytree(src, dst,
-                    ignore=shutil.ignore_patterns('.git*', 'watch_later*', '__pycache__'), dirs_exist_ok=True)
+                    ignore=shutil.ignore_patterns(
+                        '.git*', 'watch_later*', '__pycache__'),
+                    dirs_exist_ok=True)
+
+
+def main():
+    print("I am going to copy configuration files from the following directori\
+          es:")
+    for dir in dirs:
+        print("Copying", dir)
+        copy(dir)
+
+
+main()
