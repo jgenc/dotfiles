936b2943713d6647df0cf553a6fc79eeec809ca2
Jorgen Cani <jgen.cani@gmail.com>
2025-12-11 20:42:13 +0200

    Refactor Fish shell configuration and update fisher plugin manager
    - Removed temporary files: fishd.tmp.SQAyNP, fishd.tmp.tEhAaA, fishd.tmp.w67VON.
    - Enhanced _fzf_wrapper.fish to set sane defaults for FZF_DEFAULT_OPTS and FZF_DEFAULT_OPTS_FILE.
    - Added new function fish_user_key_bindings to integrate fzf with Fish shell.
    - Updated fisher.fish to version 4.4.5, improved plugin handling, and fixed regex for file_plugins.
    - Introduced new coolbeans.theme for improved color customization in Fish shell.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,8 +4,8 @@ end
 
 # Environment vars
 set -gx EDITOR /usr/bin/vim
-set -gx FLYCTL_INSTALL "/home/jorgen/.fly" 
-set -gx DENO_INSTALL "/home/jorgen/.deno"
+#set -gx FLYCTL_INSTALL "/home/jorgen/.fly" 
+#set -gx DENO_INSTALL "/home/jorgen/.deno"
 
 # ==================================
 #           Aliases
@@ -15,7 +15,7 @@ set -gx DENO_INSTALL "/home/jorgen/.deno"
 #alias sv="sudo nvim"
 alias r="ranger"
 
-alias ru="ranger ~/Workspace/university/"
+#alias ru="ranger ~/Workspace/university/"
 
 # This is used to spawn a thin wrapper for SSH, as it barely works on its own 
 # alias s="kitty +kitten ssh"
@@ -26,14 +26,36 @@ alias gp="git push"
 alias ga="git add"
 
 # ls replacer
-alias ls="exa --icons --header"
+# exa is not maintained anymore!
+alias ls="eza --icons --header"
 
 # Python
 #alias p="python"
 
+# Docker
+alias dcd="docker compose down"
+alias dcu="docker compose up"
+alias dcb="docker compose build"
+
+# wakeonlan PCs
+alias wakeniveus="wakeonlan 74:56:3C:6E:0F:62"
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-#eval /usr/bin/conda "shell.fish" "hook" $argv | source
+# if test -f /Users/jorgen/miniconda3/bin/conda
+#     eval /Users/jorgen/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+# else
+#     if test -f "/Users/jorgen/miniconda3/etc/fish/conf.d/conda.fish"
+#         . "/Users/jorgen/miniconda3/etc/fish/conf.d/conda.fish"
+#     else
+#         set -x PATH "/Users/jorgen/miniconda3/bin" $PATH
+#     end
+# end
+status is-interactive &&
+    eval /Users/jorgen/miniconda3/bin/conda "shell.fish" "hook" $argv | source
 # <<< conda initialize <<<
 
+ulimit -Sn 4096
+
+# Starship font
+starship init fish | source
